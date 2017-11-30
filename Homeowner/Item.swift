d5b6753df0dfc3e0e3bec2f17d1cfffe06ea86ce
miPera <migperalta10@gmail.com>
2017-11-29 21:19:21 -0800

    Created convenience initializer that creates random Item instance

--- Homeowner/Item.swift
+++ Homeowner/Item.swift
@@ -17,4 +17,32 @@ class Item: NSObject {
         
         super.init()
     }
+    
+    /**
+        Convenience initializer that creates a randomly generated Item instance
+    */
+    convenience init(random: Bool = false) {
+        if random {
+            let adjectives = ["Fluffy", "Rusty", "Shiny"]
+            let nouns = ["Bear", "Spork", "Mac"]
+            
+            //pick random adjective
+            var idx = arc4random_uniform(UInt32(adjectives.count))
+            let randomAdjective = adjectives[Int(idx)]
+            
+            //pick random noun
+            idx = arc4random_uniform(UInt32(nouns.count))
+            let randomNoun = nouns[Int(idx)]
+            
+            //create random name, dollar in values and serial number
+            let randomName = "\(randomAdjective) \(randomNoun)"
+            let randomValue = Int(arc4random_uniform(100))
+            let randomSerialNumber = UUID().uuidString.components(separatedBy: "-").first!
+            
+            //invoke designated initializer
+            self.init(name: randomName, serialNumber: randomSerialNumber, valueInDollars: randomValue)
+        } else {
+            self.init(name: "", serialNumber: nil, valueInDollars: 0)
+        }
+    }
 }
