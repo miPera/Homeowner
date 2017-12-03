664379611a356a263f845aa400f5454c1c8fe6f7
miPera <migperalta10@gmail.com>
2017-12-02 21:40:30 -0800

    Added function to ItemStore to create new random item

--- Homeowner/ItemStore.swift
+++ Homeowner/ItemStore.swift
@@ -2,4 +2,13 @@ import UIKit
 
 class ItemStore {
     var allItems = [Item]()
+    
+    /**
+        Create new new random Item instnance
+    */
+    @discardableResult func createItem() -> Item {
+        let newItem = Item(random: true)
+        allItems.append(newItem)
+        return newItem
+    }
 }
