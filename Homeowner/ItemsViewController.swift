065623ceed197c4cd0337383b42da23472055eae
miPera <migperalta10@gmail.com>
2017-12-04 21:27:03 -0800

    Added table view method to return number of rows to display in table

--- Homeowner/ItemsViewController.swift
+++ Homeowner/ItemsViewController.swift
@@ -2,4 +2,11 @@ import UIKit
 
 class ItemsViewController: UITableViewController {
     var itemStore: ItemStore!
+    
+    /**
+        Returns number of rows table should display
+    */
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemStore.allItems.count
+    }
 }
